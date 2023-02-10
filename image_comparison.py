af64ee0c922f32ef2cfa9d724ad0f4e8d859211d
Dhruv <dhruvmistry2000@gmail.com>
2023-02-10 17:29:16 +0530

    changes

--- image_comparison.py
+++ image_comparison.py
@@ -6,7 +6,7 @@ img = cv2.imread('Images/RDJ new.jpg')
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_encoding: ndarray = face_recognition.face_encodings(rgb_img)[0]
 
-img2 = cv2.imread("images/Rdj.jpg")
+img2 = cv2.imread("images/Jeff_Bezoz.jpg")
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2: ndarray = face_recognition.face_encodings(rgb_img2)[0]
 
@@ -16,17 +16,17 @@ if result == [True]:
     from plyer import notification
     import time
 
-    # while True:
-    #     time.sleep(2)
-    #     notification.notify(
-    #         title='Alert From Police HQ',
-    #         message='Suspected person found in your area'
-    #                 '\nshowed image of suspected person',
-    #         app_icon="Notif.ico",
-    #         timeout=8,
-    #     )
-    #     cv2.imshow("Img", img)
-    #     cv2.imshow("Img 2", img2)
+    while True:
+        time.sleep(2)
+        notification.notify(
+            title='Alert From Police HQ',
+            message='Suspected person found in your area'
+                    '\nshowed image of suspected person',
+            app_icon="Notif.ico",
+            timeout=8,
+        )
+        cv2.imshow("Img", img)
+        cv2.imshow("Img 2", img2)
 
 else:
     cv2.imshow("Img", img)

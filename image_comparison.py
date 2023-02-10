527bd075a104152a78d140f8de3a947cd546e63e
dhiry <dhiryapatel@gmail.com>
2023-02-10 18:41:23 +0530

    Thursday

--- image_comparison.py
+++ image_comparison.py
@@ -6,12 +6,13 @@ img = cv2.imread('Images/RDJ new.jpg')
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_encoding: ndarray = face_recognition.face_encodings(rgb_img)[0]
 
-img2 = cv2.imread("images/Jeff_Bezoz.jpg")
+img2 = cv2.imread("images/Rdj.jpg")
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2: ndarray = face_recognition.face_encodings(rgb_img2)[0]
 
 result = face_recognition.compare_faces([img_encoding], img_encoding2)
 print("Result: ", result)
+print("Suspect matched")
 if result == [True]:
     from plyer import notification
     import time
@@ -25,8 +26,8 @@ if result == [True]:
             app_icon="Notif.ico",
             timeout=8,
         )
-        cv2.imshow("Img", img)
-        cv2.imshow("Img 2", img2)
+        #cv2.imshow("Img", img)
+        #cv2.imshow("Img 2", img2)
 
 else:
     cv2.imshow("Img", img)

723e8714fdbab6910d294bb212c0547fb1ae5202
Diya <shahdiya194@gmail.com>
2023-02-11 11:50:14 +0530

    Login pop-up window

--- mainHomePageFile.py
+++ mainHomePageFile.py
@@ -1,7 +1,10 @@
 #Import the required Libraries
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 from turtle import update
 from PIL import Image,ImageTk
+import sys
 # from matplotlib import image
 
 root = Tk()
@@ -10,12 +13,9 @@ root.geometry("2000x1070")
 f1=Frame(root,bg="black",borderwidth=4,relief=SUNKEN)
 f1.pack(side=TOP,fill="x")
 
-def home():
-    update
-
-def data_Upload():
-    root.destroy()
-    import dataUpload
+# def home():
+#     update
+# sys.path.insert(0,'../')
 
 def footage_Upload():
     root.destroy()
@@ -23,25 +23,81 @@ def footage_Upload():
 
 def Live_CCTV():
     root.destroy()
+    # sys.path.append('./')
     import Live
 
-def login():
+# def login():
+#     root.destroy()
+#     import login 
+
+def data_upload():
     root.destroy()
-    import login 
+    import dataUpload
 
 def about_us():
     pass
 
+def home():
+    root.destroy()
+    import mainHomePageFile 
 
-# b2=Label(root,text="Facio", font=("Roboto 24 bold"), bg="black", fg="white").pack(side=TOP, anchor="nw",expand=True,padx=(10,0))
+def submit():
+    pass
 
-b2=Button(f1,fg="white",bg="black",text="Facio",font=("Roboto 24 bold"))
+def login():
+    top= Toplevel(root)
+    top.geometry("1500x350")
+    # top.config(bg="#447c84")
+    top.title("Login")
+   # frame
+    frame = Frame(top, padx=50, pady=50, height=300, width=100)
+    frame.pack_propagate(False)
+    frame.pack(expand=True)
+    Label(
+        frame, 
+        text="Admin Login", 
+        font=("Times", "30", "bold") 
+        ).grid(row=0,  columnspan=3, pady=20) #..place(x=170, y=10)
+
+    Label(
+        frame, 
+        text='Enter Username', 
+        font=("Times", "17")
+        ).grid(row=1, column=1, pady=5) #.place(x=50, y=70)
+
+    Label(
+        frame, 
+        text='Enter Password', 
+        font=("Times", "17")
+        ).grid(row=2, column=1, pady=5) #.place(x=50, y=110)
+
+    # Entry
+    uname = Entry(frame, width=70,font=("Roboto 17"))
+    pwd = Entry(frame, width=70,font=("Roboto 17"), show="*")
+    # uname.place(x=220, y=70)
+    # pwd.place(x=220, y=110)
+    uname.grid(row=1, column=2)
+    pwd.grid(row=2, column=2)
+
+    # button 
+    sub = Button(
+        frame, 
+        text="Login", 
+        padx=20, 
+        pady=10, 
+        relief=RAISED, 
+        font=("Roboto", "14", "bold"), 
+        command=submit
+        )
+    sub.grid(row=6, columnspan=3)
+
+b2=Button(f1,fg="white",bg="black",text="Facio",font=("Roboto 24 bold underline"))
 b2.pack(side=LEFT,expand=True, anchor="nw")
 
 b1=Button(f1,fg="white",bg="black",text="Home",font=("Roboto 17 bold underline"))
 b1.pack(side=LEFT,expand=True, anchor="nw",pady=(10,0))
-
-b5=Button(f1,fg="white",bg="black",text="Data Upload",font=("Roboto 17 bold"),command=data_Upload)
+    
+b5=Button(f1,fg="white",bg="black",text="Data Upload",font=("Roboto 17 bold"),command=data_upload)
 b5.pack(side=LEFT,expand=True, anchor="nw",pady=(10,0))
 # b1.grid(padx=(500,0),pady=(0,0))
 
@@ -66,7 +122,7 @@ frame = Frame(
     )
 # scrollbar = Scrollbar(root)
 # scrollbar.pack(side=RIGHT,fill=Y)
-canvas= Canvas(root, width= 1700, height= 2000)
+canvas= Canvas(root, width= 1700, height= 1500)
 # scrollbar.config(command=canvas.yview)
 canvas.pack()
 
@@ -80,7 +136,7 @@ bg=canvas.create_image(260,10, anchor=NW, image=new_image)
 def cmd(event):
     root.destroy()
     import dataUpload
-img1=(Image.open("photo2.png"))
+img1=(Image.open("photo2.png"))  
 
 resized_image1= img1.resize((150,40), Image.ANTIALIAS)
 new_image1= ImageTk.PhotoImage(resized_image1)
@@ -92,13 +148,15 @@ canvas.tag_bind(bg1,"<Button-1>",cmd)
 def cmd(event):
     root.destroy()
     import footageUpload 
-img1=(Image.open("photo2.png"))
+img1=(Image.open("photo2.png"))  
 
 resized_image2= img1.resize((150,40), Image.ANTIALIAS)
 new_image2= ImageTk.PhotoImage(resized_image2)
 bg2=canvas.create_image(880,800, anchor=NW, image=new_image2)
 
 # Button=canvas.create_image(370,450,image=img1)
-canvas.tag_bind(bg2,"<Button-1>",cmd) 
+canvas.tag_bind(bg2,"<Button-1>",cmd)  
+
+
 
 root.mainloop()
